afac54da6a00e62bb86d2130340dcd4cf63b3d39
jun.chen <cj2436586>
2022-08-01 17:17:19 +0800

    apm-webapp 网关配置无效，改为代码里去做路由

--- apm-webapp/src/main/java/org/apache/skywalking/oap/server/webapp/ApplicationStartUp.java
+++ apm-webapp/src/main/java/org/apache/skywalking/oap/server/webapp/ApplicationStartUp.java
@@ -19,9 +19,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class ApplicationStartUp {
     public static void main(String[] args) {
